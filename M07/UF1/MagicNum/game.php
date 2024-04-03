71a84f7f7ed5701ac532adc9076d3679544c5da8
AnaGmzPstr <ana.gomez.7e6@itb.cat>
2024-04-03 10:45:01 +0200

    inici del núermo mágic (no funciona)

--- M07/UF1/MagicNum/game.php
+++ M07/UF1/MagicNum/game.php
@@ -7,7 +7,28 @@
 </head>
 <body>
     <h1>Adivina el número màgic</h1>
-    <p>Acabo de pensar en un número entre el 0 i el 10 (inclosos). Esbrina'l!</p>
-    
+    <p>Acabo de pensar en un número entre el 0 y el 10 (incluidos). ¡Adivínalo!</p>
+    <?php
+    // Genera el número secreto solo si no está establecido ya
+    if (!isset($numSecreto)) {
+        $numSecreto = rand(0,10);
+        echo "$numSecreto<br>";
+    }
+
+    if(isset($_GET['submit'])) {
+        $numInput = $_GET['numero'];
+        if ($numInput == $numSecreto) {
+            echo "¡Has acertado!";
+        } elseif ($numInput > $numSecreto) {
+            echo "El número es más grande";
+        } else {
+            echo "El número es menor";
+        }
+    }
+    ?>
+    <form method="get" action="">
+        <input type="text" id="numero_magico" name="numero" max="10" />
+        <button type="submit" name="submit">Envia</button>
+    </form>
 </body>
 </html>
