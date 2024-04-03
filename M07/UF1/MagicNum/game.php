20141972fd2876f7638d1f5736934c5fc61fdb83
AnaGmzPstr <ana.gomez.7e6@itb.cat>
2024-04-03 09:45:13 +0200

    Exercici número magic creat

---
+++ M07/UF1/MagicNum/game.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magic Num</title>
+</head>
+<body>
+    <h1>Adivina el número màgic</h1>
+    <p>Acabo de pensar en un número entre el 0 i el 10 (inclosos). Esbrina'l!</p>
+    
+</body>
+</html>
